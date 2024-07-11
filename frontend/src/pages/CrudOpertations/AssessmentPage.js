24b449961af43807b1828518ec4367decff6cfd5
Your Name <you@example.com>
2024-07-11 17:39:54 +0530

    asdfxxx

--- frontend/src/pages/CrudOpertations/AssessmentPage.js
+++ frontend/src/pages/CrudOpertations/AssessmentPage.js
@@ -179,7 +179,7 @@ function AssessmentPage() {
                 }
             });
 
-            const response = await fetch('https://confess-data-tool-backend.vercel.app/api/results', {
+            const response = await fetch('https://confess-data-tool-backend.vercel.app/api/submitresults', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
